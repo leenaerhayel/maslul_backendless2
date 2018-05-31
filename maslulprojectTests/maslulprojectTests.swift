93a101d76773559aabce7ce96163d8fbbc0ef975
Hackeru_Student <hackeru@hackeru-students-mac.local>
2018-05-31 02:24:03 -0700

    Initial Commit

---
+++ maslulprojectTests/maslulprojectTests.swift
@@ -0,0 +1,36 @@
+//
+//  maslulprojectTests.swift
+//  maslulprojectTests
+//
+//  Created by Hackeru_Student on 5/31/18.
+//  Copyright © 2018 Leena Erhayel. All rights reserved.
+//
+
+import XCTest
+@testable import maslulproject
+
+class maslulprojectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
